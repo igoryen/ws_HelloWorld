0ef7f5f0f50022fb2df4466c6db552a4e8222f37
igoryen <igor.yentaltsev@gmail.com>
2014-07-10 20:44:52 -0400

    Make HelloWorld a WS v3

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -1,7 +1,14 @@
 package helloWorld;
-//v2
 
+import javax.jws.WebService;
+
+//v3
+
+@WebService
 public class HelloWorld {
+
+  public void constructor(){
+  }
   
   //v2
   public String Hello(String s){
